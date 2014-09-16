0a04d18dc474dd7a670dfc20eccb1a5259265d39
theonlykylee <kylemchalebdelacruz@Kyles-MacBook-Pro-5.local>
2014-09-16 21:20:17 +0800

    New Driver
    This is to accommodate the different files for multithreading.

---
+++ src/Driver.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+
+
+public class Driver {
+	public static void main (String args[]){
+		Scanner s = new Scanner(System.in);
+		
+		// big prime: 472882049
+		
+		System.out.print("Please enter a number to check if it is prime: ");
+		
+		BigInteger prime = s.nextBigInteger();
+		
+		//new MultiThread1(prime).threadIt();
+		//new MultiThread2(prime).threadIt();
+		new MultiThread3(prime).threadIt();
+		//new MultiThread4(prime).threadIt();
+		//new MultiThread5(prime).threadIt();
+		
+	}
+}
